60a08097842981e2bf73710100ef41e55550a98f
Pepe-f <nik.kopylov.8080@mail.ru>
2022-03-24 00:53:24 +0300

    create: slider

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 $(".banner").parallax({ imageSrc: "../img/banner-bg.jpg", speed: 0.7 });
 $(".offer").parallax({ imageSrc: "../img/banner-bg-2.jpg", speed: 0.7 });
+
+const offerSlider = new Swiper(".offer-slider", {
+  // Optional parameters
+  loop: true,
+
+  pagination: {
+    el: ".offer-slider__pagination",
+    type: "bullets",
+    clickable: true,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
